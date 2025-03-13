c29a476ce3b2b3a811719d6e91d47e782f160138
some.yummy.nick <some.yummy.nick@gmail.com>
2025-03-13 13:32:27 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import Header from '@/components/Header.vue'
+import Members from '@/components/Members.vue'
+
+function handleAdd() {}
 </script>
 
 <template>
-  <header></header>
-  <main></main>
+  <Header @handleAdd="handleAdd" />
+  <main>
+    <Members />
+  </main>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
